37aac22331b0282ca93d6da5b77e85f871097163
Andrei Ivanou <Kleckers@yandex.ru>
2019-01-31 12:43:52 +0300

    Create class AboutActivity.java

---
+++ app/src/main/java/com/number7/day5switchtoanotherscreen/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.number7.day5switchtoanotherscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+    }
+}
