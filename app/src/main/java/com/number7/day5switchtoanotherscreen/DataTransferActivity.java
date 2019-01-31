712317829ddcef5c1fd247f55bd86f84c9446d29
Andrei Ivanou <Kleckers@yandex.ru>
2019-01-31 14:06:23 +0300

    update DataTransferActivity.java update MainActivity.java update activity_data_transver.xml update activity_main.xml update string.xml

--- app/src/main/java/com/number7/day5switchtoanotherscreen/DataTransferActivity.java
+++ app/src/main/java/com/number7/day5switchtoanotherscreen/DataTransferActivity.java
@@ -2,6 +2,7 @@ package com.number7.day5switchtoanotherscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DataTransferActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class DataTransferActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_transfer);
+
+        // Значения по умолчанию
+        String username = "Asdasd";
+        String gift = "sdsa";
+
+        username = getIntent().getStringExtra("u");
+        gift = getIntent().getStringExtra("g");
+
+        TextView textViewDataTransfer = findViewById(R.id.textViewDataTransfer);
+        textViewDataTransfer.setText(username + ", отправил вам следующее сообщение: " + gift);
+
     }
 }
--- app/src/main/java/com/number7/day5switchtoanotherscreen/MainActivity.java
+++ app/src/main/java/com/number7/day5switchtoanotherscreen/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextWho;
+    private EditText editTextDescription;
+    private TextView textViewDataTransfer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextWho = findViewById(R.id.editTextWho);
+        editTextDescription = findViewById(R.id.editTextDescription);
+        textViewDataTransfer = findViewById(R.id.textViewDataTransfer);
     }
 
 
@@ -18,4 +28,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, AboutActivity.class);
         startActivity(intent);
     }
+
+    public void onClickSend(View view) {
+        Intent intent = new Intent(MainActivity.this, DataTransferActivity.class);
+        intent.putExtra("u", editTextWho.getText().toString());
+        intent.putExtra("g", editTextDescription.getText().toString());
+        startActivity(intent);
+    }
+
 }
