d5197a0ff6c06f33034adf79d4e2e14fce0b3215
Andrei Ivanou <Kleckers@yandex.ru>
2019-01-31 13:05:37 +0300

    create class DataTransferActivity.java add activity_data_transfer.xml update strings.xml update AndroidManifest.xml

---
+++ app/src/main/java/com/number7/day5switchtoanotherscreen/DataTransferActivity.java
@@ -0,0 +1,13 @@
+package com.number7.day5switchtoanotherscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataTransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_transfer);
+    }
+}
